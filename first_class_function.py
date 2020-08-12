9b30fd8aefd94a61c0e2242cef68b4ed11f42593
jangyu.hong <jangyu.hong@gmail.com>
2020-08-12 10:42:09 +0900

    first_class_function updated

--- first_class_function.py
+++ first_class_function.py
@@ -1,5 +1,10 @@
 """
     first class function에 관련된 예제 입니다.
+    변수나 데이터 구조 안에 할당 가능
+    함수 인수 전달 가능
+    함수 결과로 반환 가능
+    위 세가지 조건이 충족되면 first class function
+    파이썬의 모든 것은 객체이며 파이썬은 일급 객체 언어이다
 """
 def square(num: int) -> int:
     """
@@ -22,3 +27,13 @@ NUM_LIST = [1, 2, 3, 4, 5]
 SQUARES = my_map(square, NUM_LIST)
 
 print(SQUARES)
+
+def hello_world(num):
+    """
+        객체할당 예제
+    """
+    for _ in range(num):
+        print("Hello World")
+
+GREET = hello_world
+GREET(3)
